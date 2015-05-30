a6bd4f31829510533d1cd5ee5898d7cb1eb56bae
Fabio Arciniegas <fab.arciniegas@gmail.com>
2015-05-30 09:22:18 -0700

    tasks as simple 1 min loops

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,33 @@
 class Task < ActiveRecord::Base
+  attr_reader :start_time, :end_time
+
+  def after_create
+    @start_time = 0
+    @end_time = 0
+  end
+
+  def work
+    @not_working = true
+    Thread.new do
+      @start_time = Time.new
+      #TODO: log this time both to the app log and the appropriate model for UI display
+      @end_time = @start_time+60
+      sleep(2)
+      while completion < 99
+        print "completion #{completion}\n"
+        sleep(2)
+      end
+      #TODO: make duration customizable
+    end
+    @not_working = false
+  end
+
+  def completion
+    return 0 if @end_time.nil?
+    current_time = Time.new
+    total = @end_time - @start_time
+    left = @end_time - current_time
+    (left/total)*100
+  end
+
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -4,4 +4,19 @@ class TaskTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @task = Task.new(:name => 'test')
+  end
+
+ def test_accessor
+   assert_equal @task.name,'test'
+ end
+
+ def test_completion
+   assert_equal @task.completion,0
+   @task.work
+   sleep(2)
+   assert_not_equal @task.completion,0
+ end
+
 end
