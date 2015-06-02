767a0c1d85ba96b5ff1aefcc93fab9782c680e1f
Fabio Arciniegas (IS-US) <fabioa@US-FABIO-MAC.local>
2015-06-02 08:04:04 -0700

    random fails on /index.html (on purpose for aws elb health checks)

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,9 +1,18 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
+
+
+  # returning a broken index.html allows aws to count an instance as
+  # unresponsive
+  test "index.html should return ok sometimes, and sometimes broken" do
+    r = Hash.new
+    100.times do
+      response = get :index
+      r[response] = r[response].nil? ? 1 : 1 + r[response]
+      assert_not_equal r[:success], 0
+      assert_not_equal r[:success], 100
+    end
   end
 
 end
